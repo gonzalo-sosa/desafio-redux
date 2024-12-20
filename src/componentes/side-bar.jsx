83da6b543897829712a9f335c2284c74a88d7c9d
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-12-20 16:18:25 -0300

    Navegación lateral

--- src/componentes/side-bar.jsx
+++ src/componentes/side-bar.jsx
@@ -21,12 +21,11 @@ const SideBar = ({ items, children }) => {
   };
 
   return (
-    <aside
-      className="d-flex flex-column p-3 bg-dark"
-      style={{ width: '250px', height: '100vh' }}
-    >
-      <ul className="nav nav-pills flex-column mb-auto">{renderItems()}</ul>
-      {children}
+    <aside className="aside d-flex flex-column p-3 bg-dark">
+      <ul className="nav nav-pills flex-column mb-auto">
+        {renderItems()}
+        {children}
+      </ul>
     </aside>
   );
 };
