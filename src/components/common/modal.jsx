3e68a5b1c25361d75739f098dc69b047f1129de6
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-12-23 15:22:12 -0300

    Actualizar componente Modal para aceptar propiedades de etiqueta, cierre y botón de guardar

--- src/components/common/modal.jsx
+++ src/components/common/modal.jsx
@@ -1,17 +1,18 @@
 import { createPortal } from 'react-dom';
 
-const Modal = ({ children }) => {
+const Modal = ({ label, onClose, btnSave, children }) => {
   return createPortal(
-    <div className="modal" tabIndex="-1">
+    <div className="modal" tabIndex="-1" style={{ display: 'block' }}>
       <div className="modal-dialog">
         <div className="modal-content">
           <div className="modal-header">
-            <h5 className="modal-title">Modal title</h5>
+            <h5 className="modal-title">{label}</h5>
             <button
               type="button"
               className="btn-close"
               data-bs-dismiss="modal"
               aria-label="Close"
+              onClick={() => onClose()}
             ></button>
           </div>
           <div className="modal-body">{children}</div>
@@ -20,10 +21,16 @@ const Modal = ({ children }) => {
               type="button"
               className="btn btn-secondary"
               data-bs-dismiss="modal"
+              aria-label="Close"
+              onClick={() => onClose()}
             >
               Close
             </button>
-            <button type="button" className="btn btn-primary">
+            <button
+              className="btn btn-primary"
+              aria-label="Save changes"
+              {...btnSave}
+            >
               Save changes
             </button>
           </div>
