c35597db521d65d77cf6f0bb0d31c958c67f883f
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-12-23 14:15:12 -0300

    Uso de middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducer from './reducer.js';
 import api from './middleware/api.js';
+import * as auth from './middleware/auth.js';
+import * as toast from './middleware/toast.js';
 
 export default function () {
   return configureStore({
     reducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api),
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(...auth, api, ...toast),
   });
 }
