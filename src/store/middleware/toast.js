34e714f8487123f47ac5b2e3bf803717db8364fd
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-12-23 14:10:36 -0300

    Toast middleware

--- src/store/middleware/toast.js
+++ src/store/middleware/toast.js
@@ -1,7 +1,13 @@
 /* eslint-disable no-unused-vars */
 import { toast } from 'react-toastify';
+import * as actions from '../toast.js';
 
 export const toastMiddleware = (store) => (next) => (action) => {
-  if (action.type === 'error') toast.error(action.payload.message);
-  else return next(action);
+  if (action.type === actions.notifyErrorToast.type) {
+    toast.error(action.payload.message);
+  }
+  if (action.type === actions.notifyToast.type) {
+    toast(action.payload.message);
+  }
+  return next(action);
 };
