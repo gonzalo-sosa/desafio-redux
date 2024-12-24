baf1b0c83338025148fd2b034381ded19c41f4e7
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-12-24 10:39:23 -0300

    Añadido archivo .prettierignore y configuraciones para Prettier y ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 export default [
-  { ignores: ['dist', 'tests'] },
+  { ignores: ["node_modules", 'dist', 'tests'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
