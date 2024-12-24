15d8fb93ba55de3d0beefdea96ff42ffa44fe241
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-12-24 11:57:41 -0300

    Añadido reducer de usuarios

--- src/store/entities.js
+++ src/store/entities.js
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import boardsReducer from './boards.js';
 import tasksReducer from './tasks.js';
+import usersReducer from './users.js';
 
 export default combineReducers({
   boards: boardsReducer,
   tasks: tasksReducer,
+  users: usersReducer,
 });
