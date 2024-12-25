073ad745d04612e69e6ec8a94bbac3e4946139e4
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-12-25 16:32:43 -0300

    Conectar componente con contexto de store

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,18 +1,35 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
+import NavBar from '@/components/nav-bar';
+import { connect } from 'react-redux';
+import { getBoardById, removeBoard, updateBoard } from './../store/boards';
+import { getTasksByBoardId } from '../store/tasks';
 
 class Board extends Component {
   render() {
     return (
       <main className="main p-3">
-        <h2>Board {this.props.board.id}</h2>
+        <NavBar />
+        <section>
+          <h2>{JSON.stringify(this.props.board)}</h2>
+        </section>
       </main>
     );
   }
 }
 
 Board.propTypes = {
-  board: PropTypes.object.isRequired,
+  board: PropTypes.object,
 };
 
-export default Board;
+const mapStateToProps = (state, ownProps) => ({
+  board: getBoardById(state, ownProps.id),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  updateBoard: (data) => dispatch(updateBoard(data)),
+  removeBoard: (id) => dispatch(removeBoard(id)),
+  getTasksByBoardId: (id) => dispatch(getTasksByBoardId(id)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Board);
