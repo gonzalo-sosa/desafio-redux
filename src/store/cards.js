6133128f71de4b62f958c52586b0c617ca2f6136
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-12-26 15:42:52 -0300

    Agregar gestión de tarjetas en el store

---
+++ src/store/cards.js
@@ -0,0 +1,69 @@
+/* eslint-disable no-unused-vars */
+import { createSelector } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+
+let lastId = 0;
+
+const cardSlice = createSlice({
+  name: 'cards',
+  initialState: { list: [] },
+  reducers: {
+    cardAdded: (cards, action) => {
+      const card = { ...action.payload, id: ++lastId };
+      cards.list.push(card);
+    },
+    cardUpdated: (cards, action) => {
+      let card = cards.list.find((card) => card.id === action.payload.id);
+      Object.assign(card, action.payload);
+    },
+    cardRemoved: (cards, action) => {
+      cards.list = cards.list.filter((card) => card.id !== action.payload.id);
+    },
+    allCardsRemoved: (cards, action) => {
+      cards.list = [];
+      lastId = 0;
+    },
+    allCardsRemovedFromTask: (cards, action) => {
+      cards.list = cards.list.filter(
+        (card) => card.taskId !== action.payload.taskId,
+      );
+    },
+  },
+});
+
+const {
+  cardAdded,
+  cardUpdated,
+  cardRemoved,
+  allCardsRemoved,
+  allCardsRemovedFromTask,
+} = cardSlice.actions;
+export default cardSlice.reducer;
+
+export const addCard = (card) => (dispatch) => {
+  dispatch(cardAdded(card));
+};
+
+export const updateCard = (card) => (dispatch) => {
+  dispatch(cardUpdated(card));
+};
+
+export const removeCard = (card) => (dispatch) => {
+  dispatch(cardRemoved(card));
+};
+
+export const removeAllCards = () => (dispatch) => {
+  dispatch(allCardsRemoved());
+};
+
+export const removeAllCardsFromTask = (task) => (dispatch) => {
+  dispatch(allCardsRemovedFromTask(task));
+};
+
+export const getCards = createSelector(
+  (state) => state.entities.cards,
+  (cards) => cards.list,
+);
+
+export const getCardsByTaskId = (state, taskId) =>
+  getCards(state).filter((card) => card.taskId === taskId);
--- src/store/entities.js
+++ src/store/entities.js
@@ -1,10 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
+import usersReducer from './users.js';
 import boardsReducer from './boards.js';
 import tasksReducer from './tasks.js';
-import usersReducer from './users.js';
+import cardsReducer from './cards.js';
 
 export default combineReducers({
+  users: usersReducer,
   boards: boardsReducer,
   tasks: tasksReducer,
-  users: usersReducer,
+  cards: cardsReducer,
 });
