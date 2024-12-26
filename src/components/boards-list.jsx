782f9ab4c39346b702bf39a467e47f5dc1829db3
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-12-26 15:45:28 -0300

    Refactor BoardsList para que sea un componente de clase y conectarlo a Redux para el manejo de los tableros

--- src/components/boards-list.jsx
+++ src/components/boards-list.jsx
@@ -1,50 +1,75 @@
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { loadBoards } from '../store/boards';
+import { getBoards } from '../store/boards';
 import NewBoardForm from './new-board-form';
 import { NavLink } from 'react-router-dom';
 import Modal from './common/modal';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Component } from 'react';
 
-const BoardsList = () => {
-  const dispatch = useDispatch();
-  const boards = useSelector((state) => state.entities.boards.list);
-  const [showForm, setShowForm] = useState(false);
+class BoardsList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showForm: false,
+    };
+  }
 
-  useEffect(() => {
-    dispatch(loadBoards());
-  }, [dispatch]);
+  render() {
+    const { boards } = this.props;
 
-  return (
-    <>
-      <button className="btn btn-primary" onClick={() => setShowForm(true)}>
-        +
-      </button>
-      <ul>
-        {showForm && (
-          <Modal
-            label={'Nuevo tablero'}
-            onClose={() => setShowForm(false)}
-            btnSave={{
-              type: 'submit',
-              form: 'new-board-form',
-            }}
+    return (
+      <>
+        <div className="nav-link text-light d-flex flex-row justify-content-between align-items-center pe-0">
+          <span>Sus tableros</span>
+          <button
+            className="btn btn-primary"
+            onClick={() => this.setState({ showForm: true })}
           >
-            <NewBoardForm
-              onSubmit={() => setShowForm(false)}
-              form={{ id: 'new-board-form' }}
-            />
-          </Modal>
-        )}
-        {boards.map((board) => (
-          <li key={`board-${board.id}`} className="nav-item">
-            <NavLink to={`/boards/${board.id}`} className="nav-link text-light">
-              {board.title}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+            +
+          </button>
+        </div>
+        <ul>
+          {this.state.showForm && (
+            <Modal
+              label={'Nuevo tablero'}
+              onClose={() => this.setState({ showForm: false })}
+              btnSave={{
+                type: 'submit',
+                form: 'new-board-form',
+              }}
+            >
+              <NewBoardForm
+                onSubmit={() => this.setState({ showForm: false })}
+                form={{ id: 'new-board-form' }}
+              />
+            </Modal>
+          )}
+          {boards.map((board) => (
+            <li key={`board-${board.id}`} className="nav-item">
+              <NavLink
+                to={`/boards/${board.id}`}
+                className="nav-link text-light"
+              >
+                {board.title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
+
+BoardsList.propTypes = {
+  boards: PropTypes.array,
 };
 
-export default BoardsList;
+const mapStateToProps = (state) => ({
+  boards: getBoards(state),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  dispatch,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(BoardsList);
