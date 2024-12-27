ab78e2ae60f9408f5d25f26cdda6470d1900775b
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-12-27 10:05:53 -0300

    Agregado path alias para utils

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     alias: {
       "@/components": path.resolve(__dirname, "./src/components"),
       "@/store": path.resolve(__dirname, "./src/store"),
+      "@/utils": path.resolve(__dirname, "./src/utils"),
     },
   },
 });
