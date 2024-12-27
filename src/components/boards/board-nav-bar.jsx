bfaabddf3f7121ffb25a97d7e99c749f075a50ed
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-12-27 10:09:25 -0300

    Agregar componente BoardNavBar para la navegación en tableros

---
+++ src/components/boards/board-nav-bar.jsx
@@ -0,0 +1,90 @@
+import { Component } from 'react';
+import ArrowIcon from '@/components/common/icons/arrow-icon';
+import BarsIcon from '@/components/common/icons/bars-icon';
+import DotsIcon from '@/components/common/icons/dots-icon';
+import GroupIcon from '@/components/common/icons/group-icon';
+import LightningIcon from '@/components/common/icons/lightning-icon';
+import NavBar from '@/components/common/nav-bar';
+import PropTypes from 'prop-types';
+import RocketIcon from '@/components/common/icons/rocket-icon';
+import ShareIcon from '@/components/common/icons/share-icon';
+import StarIcon from '@/components/common/icons/star-icon';
+
+class BoardNavBar extends Component {
+  render() {
+    const { title } = this.props;
+
+    return (
+      <NavBar>
+        <span className="navbar-brand text-white">{title}</span>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">
+              <StarIcon />
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">
+              <GroupIcon />
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">
+              Tablero
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">
+              Tabla
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">
+              <ArrowIcon />
+            </a>
+          </li>
+        </ul>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">
+              <RocketIcon />
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">
+              <LightningIcon />
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">
+              <BarsIcon />
+              Filtros
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">
+              Cuenta
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">
+              <ShareIcon />
+              Compartir
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">
+              <DotsIcon />
+            </a>
+          </li>
+        </ul>
+      </NavBar>
+    );
+  }
+}
+
+BoardNavBar.propTypes = {
+  title: PropTypes.string,
+};
+
+export default BoardNavBar;
