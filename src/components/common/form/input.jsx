5f64257773001193eac34e0fea13f6e19f1e26ff
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-12-27 10:09:04 -0300

    Agregar verificación para mostrar etiqueta en el componente Input

--- src/components/common/form/input.jsx
+++ src/components/common/form/input.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 const Input = ({ name, label, error, children, ...rest }) => {
   return (
     <div className="form-group">
-      <label htmlFor={name}>{label}</label>
+      {label && <label htmlFor={name}>{label}</label>}
       <input {...rest} id={name} name={name} className="form-control" />
       {children}
       {error && <div className="alert alert-danger">{error}</div>}
